db76eae858ea7e94168ab36d5af13d99cb736f19
MuhammadBasit9951 <muhammadbasit9951@gmail.com>
2024-10-21 16:33:02 +0000

    adding array and object

--- basics/test.js
+++ basics/test.js
@@ -1,3 +1,13 @@
 console.log("Basit files")
 console.log(" ok is ok")
 console.log("this is only for github repository update")
+let pakistan = ["country" ,  "Beautiful" , "heaven on earth" , 1947]
+console.log(pakistan)
+const pakistanDetail = 
+{
+name: "pakistan",
+type: "country",
+intobeing: 1947,
+about: "heaven on earth"
+}
+console.log(pakistanDetail)
